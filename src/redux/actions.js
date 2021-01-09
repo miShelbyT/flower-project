0729e006e1c7a41a5b5267ff4c8624197bb304bf
miShelbyT <mi.shelbyrose@gmail.com>
2021-01-09 18:40:39 -0500

    will come back and add POST request to actions.js later so new flower persists

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -3,7 +3,25 @@ import { ADD_FLOWER, GET_FLOWERS } from './actionTypes';
 // the whole point of this function is to return the object with type key and payload.
 
 export const addFlower = (flowerObj) => ({type: ADD_FLOWER, payload: flowerObj})
+// TODO need a POST request so new flower persists in db!!!
 
+// submitHandler = (flowerObj) => {
+  //   fetch('http://localhost:5000/flowerlist', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(flowerObj),
+  //   })
+  //     .then(response => response.json())
+  //     .then(newFlowerObj => {
+  //       this.setState({ flowerAPI: [...this.state.flowerAPI, newFlowerObj] })
+  //       console.log('Success:', newFlowerObj);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error:', error);
+  //     });
+  // }
 
 export const getFlowers = () => {
   // console.log("first dispatch invoked")
@@ -14,8 +32,6 @@ export const getFlowers = () => {
         .then(resp => resp.json())
         // .then(console.log)
         .then(flowerArray => dispatch({ type: GET_FLOWERS, payload: flowerArray }))
-
-// ...........something is wrong with line 16? everything works fine until in comment that line back in - initial fetch does not work on render................
 
 // need to pass dispatch as an argument and then use dispatch inside the second .then - inception! we are using dispatch twice. once for the original mdp and again after the data has returned in the 2nd .then.
 
